6fbc99d34386a48f1830da754418fbe97e03aad0
Wendy Le <lehong@Les-MacBook-Pro.local>
2020-02-05 16:04:02 -0500

    adding folder

---
+++ js/main.js
@@ -0,0 +1,16 @@
+(() => {
+  //set up the tracks or the characters
+
+
+
+
+
+  // set the drop zone background image
+
+  //add event handling here
+
+
+
+  //click on the tracks to change the music 
+
+})();
